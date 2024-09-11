43f3c8a15cee0bbd8065ff51eaac8c96f09a735d
juan-7j <103474094+juan-7j@users.noreply.github.com>
2024-09-11 00:51:38 -0500

    Trabajo JS

---
+++ script.js
@@ -0,0 +1,13 @@
+// Declaración de variables con let
+let edad = 25;
+let nombre = "Carlos";
+let altura = 1.75;
+let peso = 70;
+let temperatura = 36.6;
+let dinero = 100;
+let dia = "Lunes";
+let hora = 8;
+let esMayorDeEdad = true;
+let nacionalidad = "Colombiana";
+
+
